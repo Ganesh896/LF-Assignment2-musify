f1517a810f49e3cf5fc28b60c1c9a0d88b088e16
gsaud896 <gsaud896@gmail.com>
2024-06-04 10:40:30 +0545

    offer countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,21 @@ window.addEventListener("scroll", function () {
     const navBar = document.querySelector(".navbar");
     navBar.classList.toggle("sticky", this.window.scrollY > 100);
 });
+
+//Timer Countdown
+var offterTill = new Date("Aug 3, 2024").getTime();
+console.log(offterTill);
+
+var x = setInterval(function () {
+    var now = new Date().getTime();
+
+    var distance = offterTill - now;
+
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector(".hours").innerHTML = hours < 10 ? "0" + hours : hours;
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
+}, 1000);
